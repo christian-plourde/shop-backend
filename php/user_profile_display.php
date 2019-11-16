e89d5540628e9f110c10f2729fe66a0f07af5716
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-16 16:25:40 -0500

    displays all user info

--- php/user_profile_display.php
+++ php/user_profile_display.php
@@ -20,7 +20,7 @@ $username = $data['username'];
 
 $result = get_account($username);
 
-$echo_array = array("Accepted" => false, "email" => "", "firstName" => "", "lastName" => "", "country" => "", "address" => "");
+$echo_array = array("Accepted" => false, "email" => "", "firstName" => "", "lastName" => "", "country" => "", "address" => "", "isAdmin" => false);
 
 if($result == "null")
 {
@@ -33,6 +33,12 @@ else
     $echo_array["email"] = json_decode($result, TRUE)["email"];
     $echo_array["firstName"] = json_decode($result, TRUE)["firstName"];
     $echo_array["lastName"] = json_decode($result, TRUE)["lastName"];
+    $echo_array["country"] = json_decode($result, TRUE)["Country"];
+    $echo_array["address"] = json_decode($result, TRUE)["address"];
+    if(json_decode($result, TRUE)["isAdmin"] === 1)
+    	$echo_array["isAdmin"] = true;
+    else
+    	$echo_array["isAdmin"] = false;
 }
 
 echo json_encode($echo_array);
