fcb272519c33b829d40c094e03ca6538d138dac0
jack-burns <burnsj056@gmail.com>
2019-11-26 00:04:37 -0500

    more var_dumps
    var_dumps for testing

--- php/transaction.php
+++ php/transaction.php
@@ -9,6 +9,7 @@ use PHPMailer\PHPMailer\Exception;
 require '../vendor/autoload.php';
 
 function insert_transcation($productid,$buyerid,$sellerid,$quantity,$price){
+var_dump("in insert_transaction");
 var_dump($productid);
 var_dump($buyerid);
 var_dump($sellerid);
@@ -23,20 +24,32 @@ $result = $db->query("INSERT INTO Transaction(productID,buyerID,sellerID,time_st
 $db->query("UPDATE Products SET quantity = quantity - $quantity WHERE productID = $productid");
 send_buyer_email($productid, $buyerid, $sellerid, $date, $price);
 send_seller_email($productid, $sellerid, $date);
+
 }
 else
 {
+var_dump("quantity error");
 echo("Cannot sell more than available!");
 }
+var_dump("out insert_transaction");
 }
 
 function send_buyer_email($productid, $buyerid, $sellerid, $date, $price) {
+	var_dump("in send_buyer_email");
 	$db = get_db_connection();
 	$buyerEmail = $db->query("SELECT email FROM Accounts WHERE accountID = $buyerid")->fetch_assoc()['email'];
 	$buyerUser = $db->query("SELECT firstName FROM Accounts WHERE accountID = $buyerid")->fetch_assoc()['firstName'];
 	$sellerName = $db->query("SELECT username FROM Accounts WHERE accountID = $sellerid ")->fetch_assoc()['username'];
 	$prodName = $db->query("SELECT productName FROM Products WHERE productID = $productid")->fetch_assoc()['productName'];
 	$priceAfterTax = $price * 1.15;
+
+	var_dump($buyerEmail);
+	var_dump($buyerUser);
+	var_dump($sellerName);
+	var_dump($prodName);
+	var_dump($price);
+	var_dump($priceAfterTax);
+
 
 	$subject = "Thank you for your purchase from $sellerName";
 	$message = "Hello $buyerUser,<br>
@@ -53,18 +66,26 @@ function send_buyer_email($productid, $buyerid, $sellerid, $date, $price) {
 	$secureCheck = sanitize_email($buyerEmail);
 	if($secureCheck == false) {
 		echo("Invalid Email");
+		var_dump("send_buyer_email invalid");
 	} else {
 		send_mail($buyerEmail, $subject, $message);
+		var_dump("send_buyer_email sent");
 		echo("email sent");
 	}
-
+	var_dump("out send_buyer_email");
 }
 
 function send_seller_email($productid, $sellerid, $date) {
+	var_dump("in send_seller_email");
 	$db = get_db_connection();
 	$sellerEmail = $db->query("SELECT email FROM Accounts WHERE accountID = $sellerid ")->fetch_assoc()['email'];
 	$sellerName = $db->query("SELECT username FROM Accounts WHERE accountID = $sellerid ")->fetch_assoc()['username'];
 	$prodName = $prodName = $db->query("SELECT productName FROM Products WHERE productID = $productid")->fetch_assoc()['productName'];
+
+	var_dump($sellerEmail);
+	var_dump($sellerName);
+	var_dump($prodName);
+
 
 	$subject = "Your product has been bought!";
 	$message = "Hello, $sellerName,<br>
@@ -74,14 +95,18 @@ function send_seller_email($productid, $sellerid, $date) {
 	$secureCheck = sanitize_email($sellerEmail);
 	if($secureCheck == false) {
 		echo ("Invalid Email");
+		var_dump("send_seller_email invalid");
 	} else {
 		send_mail($sellerEmail, $subject, $message);
+		var_dump("send_seller_email sent");
 		echo ("email sent");
 	}
+	var_dump("out send_seller_email");
 }
 
 
 function sanitize_email($email) {
+	var_dump("in sanitize_email");
 	$email = filter_var($email, FILTER_SANITIZE_EMAIL);
 	if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
 		return true;
@@ -92,6 +117,7 @@ function sanitize_email($email) {
 
 
 function send_mail($email, $subject, $message) {
+	var_dump("in send_mail");
 	$mail = new PHPMailer(true);
     try {
         $mail->SMTPOptions = array(
@@ -128,11 +154,15 @@ function send_mail($email, $subject, $message) {
         $mail->send();
 		$result["msg"] = "GOOD";
 		echo "good";
+		var_dump("mail sent");
         echo json_encode($result);
     } catch (Exception $e) {
-        $result["msg"] = "error $mail->ErrorInfo";
-        echo json_encode($result);
-    }
+		$result["msg"] = "error $mail->ErrorInfo";
+		var_dump("mail failed");
+		var_dump($mail->ErrorInfo);
+		echo json_encode($result);
+	}
+	var_dump("out send_mail");
 }
 
 
