9c3cbf82f93581e27ed203d790772f14a973100e
David Bod <Davidbodromian@outlook.com>
2019-11-25 19:52:14 -0500

    refactoring
    we now return an associative array rather than an object

--- php/Admin.php
+++ php/Admin.php
@@ -8,16 +8,20 @@ function get_best_sellers_by_quantity(){
                             FROM Transaction 
                             GROUP BY productID 
                             ORDER BY AMOUNT DESC");
+    $array = $result->fetch_all(MYSQLI_ASSOC);
+    return json_encode($array);
 }
 
 // will only consider products that have more than 3 reviews
 function get_best_sellers_by_reviews(){
     $db = get_db_connection();
-    $result = $db->query("SELECT productID, Count(rating) AS ratingCount, sum(rating)/Count(rating) as ratingAverage
+    $result = $db->query("SELECT productID, Count(rating) AS ratingCount, avg(rating) as ratingAverage
                             FROM Reviews
                             GROUP BY productID
                             HAVING ratingCount > 3
                             ORDER BY ratingAverage DESC, ratingCount");
+    $array = $result->fetch_all(MYSQLI_ASSOC);
+    return json_encode($array);
 }
 
 ?>
