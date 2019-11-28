4d74d8bdadc042d03c3dc750538aa590a3df5ae7
TheyCallMeJQ <jacques.a@libofthings.org>
2019-11-28 01:46:08 -0500

    another require_once Connect.php in user_profile_display.php

--- php/user_profile_display.php
+++ php/user_profile_display.php
@@ -1,13 +1,5 @@
 <?php
-function get_db_connection() {
-  $servername = "www.remotemysql.com";
-  $username = "HQgsxOVVFA";
-  $password = "QU8LU8QaqR";
-  $database = "HQgsxOVVFA";
-  $dbport = "3306";
-  $dbh = new mysqli($servername, $username, $password, $database, $dbport);
-  return $dbh;
-}
+require_once("Connect.php");
 
 function get_account($user){
     $db = get_db_connection();
