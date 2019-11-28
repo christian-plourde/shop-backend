72b7c3afb13c9cb166ec3bb7b97a01e4a7c839f3
TheyCallMeJQ <jacques.a@libofthings.org>
2019-11-27 22:18:34 -0500

    Made some tweaks to the login.php

--- php/login.php
+++ php/login.php
@@ -1,11 +1,11 @@
 <?php
 function get_db_connection() {
-$servername = "www.remotemysql.com";
-$username = "HQgsxOVVFA";
-$password = "QU8LU8QaqR";
-$database = "HQgsxOVVFA";
-$dbport = "3306";
-$dbh = new mysqli($servername, $username, $password, $database, $dbport);
+  $servername = "www.remotemysql.com";
+  $username = "HQgsxOVVFA";
+  $password = "QU8LU8QaqR";
+  $database = "HQgsxOVVFA";
+  $dbport = "3306";
+  $dbh = new mysqli($servername, $username, $password, $database, $dbport);
 return $dbh;
 }
 
@@ -14,6 +14,8 @@ function get_account($user, $pass){
     $result = $db->query("SELECT * FROM Accounts WHERE Accounts.username = '$user' AND Accounts.password = '$pass'");
     return json_encode ($result->fetch_assoc());
     }
+
+
 
 $data = json_decode(file_get_contents("php://input"), TRUE);
 $username = $data['username'];
@@ -29,9 +31,9 @@ if(!empty($username) && !empty($password)){
 
         $echo_array["Accepted"] = false;
         echo json_encode($echo_array);
-         
+
     } else {
-        
+
         $echo_array["Accepted"] = true;
         echo json_encode($echo_array);
     }
