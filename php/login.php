cce6798cb8dcfcdcaecae81e4c94e87f22d90474
TheyCallMeJQ <jacques.a@libofthings.org>
2019-11-28 00:25:05 -0500

    Added all relevant files

--- php/login.php
+++ php/login.php
@@ -1,4 +1,5 @@
 <?php
+//A function to return a connection to th database
 function get_db_connection() {
   $servername = "www.remotemysql.com";
   $username = "HQgsxOVVFA";
@@ -6,36 +7,30 @@ function get_db_connection() {
   $database = "HQgsxOVVFA";
   $dbport = "3306";
   $dbh = new mysqli($servername, $username, $password, $database, $dbport);
-return $dbh;
+  return $dbh;
 }
-
+//A function to return a list of all successful queries in the Accounts table for the given username and password.
 function get_account($user, $pass){
     $db = get_db_connection();
-    $result = $db->query("SELECT * FROM Accounts WHERE Accounts.username = '$user' AND Accounts.password = '$pass'");
+    $query = "SELECT * FROM Accounts WHERE username = '$user' AND password = '$pass';";
+    $result = $db->query($query);
+
     return json_encode ($result->fetch_assoc());
     }
 
-
-
+//Get the data we sent over via our axios.post() in the login
 $data = json_decode(file_get_contents("php://input"), TRUE);
+//To make our lives easier, set variables for easy access
 $username = $data['username'];
 $password = $data['password'];
-
-
+//We assume the results are false, by default
+$echo_array = array("Accepted" => false);
 if(!empty($username) && !empty($password)){
+    //Access the database and check validity of arguments
     $result = get_account($username, $password);
-
-    $echo_array = array("Accepted" => false);
-
-    if($result == "null"){
-
-        $echo_array["Accepted"] = false;
-        echo json_encode($echo_array);
-
-    } else {
-
-        $echo_array["Accepted"] = true;
-        echo json_encode($echo_array);
-    }
+    //Set accepted to true if the values are legit, else keep to false.
+    $echo_array["Accepted"] = ($result != "null");
 }
+//Return the result; either {"Accepted":true} or {"Accepted":false}
+echo json_encode($echo_array);
 ?>
--- php/user_profile_display.php
+++ php/user_profile_display.php
@@ -1,33 +1,33 @@
 <?php
 function get_db_connection() {
-$servername = "www.remotemysql.com";
-$username = "HQgsxOVVFA";
-$password = "QU8LU8QaqR";
-$database = "HQgsxOVVFA";
-$dbport = "3306";
-$dbh = new mysqli($servername, $username, $password, $database, $dbport);
-return $dbh;
+  $servername = "www.remotemysql.com";
+  $username = "HQgsxOVVFA";
+  $password = "QU8LU8QaqR";
+  $database = "HQgsxOVVFA";
+  $dbport = "3306";
+  $dbh = new mysqli($servername, $username, $password, $database, $dbport);
+  return $dbh;
 }
 
 function get_account($user){
     $db = get_db_connection();
     $result = $db->query("SELECT * FROM Accounts WHERE Accounts.username = '$user'");
     return json_encode ($result->fetch_assoc());
-    }
+}
 
 $data = json_decode(file_get_contents("php://input"), TRUE);
-$username = $data['username'];
+if (!isset($data))
+{
+  echo "Data not set";
+  return array("Accepted" => false);
+}
 
+$username = $data['username'];
 $result = get_account($username);
 
 $echo_array = array("Accepted" => false, "email" => "", "firstName" => "", "lastName" => "", "country" => "", "address" => "", "isAdmin" => false);
 
-if($result == "null")
-{
-    $echo_array["Accepted"] = false;
-}
-
-else
+if($result != "null")
 {
     $echo_array["Accepted"] = true;
     $echo_array["email"] = json_decode($result, TRUE)["email"];
@@ -35,10 +35,7 @@ else
     $echo_array["lastName"] = json_decode($result, TRUE)["lastName"];
     $echo_array["country"] = json_decode($result, TRUE)["Country"];
     $echo_array["address"] = json_decode($result, TRUE)["address"];
-    if(json_decode($result, TRUE)["isAdmin"] === 1)
-    	$echo_array["isAdmin"] = true;
-    else
-    	$echo_array["isAdmin"] = false;
+    $echo_array["isAdmin"] = ((intval(json_decode($result, TRUE)["isAdmin"]) == 1));
 }
 
 echo json_encode($echo_array);
