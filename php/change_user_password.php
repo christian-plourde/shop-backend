92b0f7f0a75d0f67a7ec176259eafc7630a97be3
TheyCallMeJQ <jacques.a@libofthings.org>
2019-11-28 23:20:57 -0500

    Added user password change and forgot password backend functionalities

--- php/change_user_password.php
+++ php/change_user_password.php
@@ -1,32 +1,34 @@
 <?php
 include 'Connect.php';
-// include 'Encryption.php';
-$SHIFT = 1;
-
-// #What we receive is already encrypted
+//What we receive is already encrypted
+//Hardcoded values for debugging
 // $data = array(
-//   #Input field => value
-//   "new_password" => "cadabra",
-//   "username" => "Abra");
+//   "password" => "nbhpp",
+//   "username" => "dat_magoo");
 
 $data = json_decode(file_get_contents("php://input"), TRUE);
-#At this point, we assume the old password has been verified.
+if (!isset($data))
+{
+  echo array("Accepted"=>false, "reason"=>"data null");
+  return;
+}
+//Else data is set
+$USERNAME = $data['username'];
+$NEW_PASSWORD = $data['password'];
+#At this point, we assume the old password has been verified and encrypted.
 #2. Provide the new password
 #   For this, the query will have the form
 # UPDATE Accounts SET password = $new_password WHERE username = $username;
-$USERNAME = $data['username'];
-// $NEW_PASSWORD = encrypt($data['new_password'], $SHIFT);
-$NEW_PASSWORD = $data['password'];
 $QUERY = "UPDATE Accounts SET password='$NEW_PASSWORD' WHERE username = '$USERNAME';";
-// echo $QUERY;
-// return;
+
 $db = get_db_connection();
-$echo_array = array("Accepted" => false);
+$echo_array = array("Accepted" => false, "reason"=>"failed query");
 $db->query($QUERY);
 $err = $db->error;
 if($err == NULL)
 {
   $echo_array["Accepted"] = true;
+  $echo_array["reason"] = '';
 }
 echo json_encode($echo_array);
 ?>
