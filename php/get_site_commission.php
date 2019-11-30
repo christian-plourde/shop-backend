b50f6ef2baee3e8e2281d39cc70e70cfd966851c
TheyCallMeJQ <jacques.a@libofthings.org>
2019-11-30 17:09:24 -0500

    Added admin site commission.

--- php/get_site_commission.php
+++ php/get_site_commission.php
@@ -1,17 +1,24 @@
 <?php
 /*
-  A file to get all site earnings in a given time interval, should return a fixed numerical value with two decimal places. i.e. 10.00
-Note: we have a BETWEEN keyword, if we want
+  A file to get all site earnings in a given time interval
   SELECT * FROM table
-  WHERE timestamp BETWEEN '2012-05-05 00:00:00' AND '2012-05-05 23:59:59'
+  WHERE timestamp > $timestamp
 */
-require_once("User.php");
 require_once("Connect.php");
+
+function get_site_commission($timestamp){
+  $db = get_db_connection();
+  $result = $db->query("SELECT 0.08*sum(price) AS commission FROM Transaction where time_stamp > $timestamp")->fetch_all(MYSQLI_ASSOC);
+  return json_encode($result);
+}
 
 /*The following for debugging*/
 //SQL timestamp format is the following: '1970-01-01 00:00:01'
-$data = json_decode('{"from_date":"today"}', TRUE);
-// $data = json_decode(file_get_contents("php://input"), TRUE);
+// $data = json_decode('{"from_date":"\'2019-09-03 00:00:00\'"}', TRUE);
+$data = json_decode(file_get_contents("php://input"), TRUE);
+// echo $data["from_date"];
+// return;
+
 
 if (!isset($data))
 {
@@ -22,7 +29,7 @@ if (!isset($data))
 $from_date = $data['from_date'];
 
 // $result = get_products_sold_by_user($username);
-$result = get_commission_since_date($from_date);
+$result = get_site_commission($from_date);
 
 $echo_array = array("Accepted" => false,
                     "reason"=>"unsuccessful query",
@@ -31,7 +38,8 @@ if(isset($result))
 {
     $echo_array["Accepted"] = true;
     $echo_array["reason"] = "";
-    $echo_array["commission"] = $result;
+    $commission_obj = json_decode($result)[0];
+    $echo_array["commission"] = $commission_obj;
 }
 echo json_encode($echo_array);
 ?>
