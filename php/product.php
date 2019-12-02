ec17cadd4ccd2285c8b91ffd77e57d45c229902b
Hamza Kaced <thedudesrock61@gmail.com>
2019-12-02 00:56:34 -0500

    Updated files

--- php/product.php
+++ php/product.php
@@ -1,17 +1,17 @@
 <?php
 require_once ("Connect.php");
-//Gets Products; If input is -1, will return all products. If input is a number greater than zero, it will return a products with
-//the same product id as the input
+$postdata = json_encode(file_get_contents('php://input'));
+//Gets Products; If input is -1, will return all products. If input is a number greater than zero, it will return a products with the same product id as the input
 function get_products($id) {
     $db = get_db_connection();
+    $result = $db->query("SELECT Products.* FROM Products");
+    $productimgs = $db->query("SELECT Products.productID AS pid, image_url FROM ProductImages,Products WHERE Products.productID = ProductImages.productID")->fetch_all(MYSQLI_ASSOC);
+    $producttags = $db->query("SELECT productID, tag FROM Tags")->fetch_all(MYSQLI_ASSOC);
+    $array = $result->fetch_all(MYSQLI_ASSOC);
     if ($id == - 1) {
-        $productimgs = $db->query("SELECT Products.productID AS pid, image_url FROM ProductImages,Products WHERE Products.productID = ProductImages.productID")->fetch_all(MYSQLI_ASSOC);
-        $producttags = $db->query("SELECT productID, tag FROM Tags")->fetch_all(MYSQLI_ASSOC);
-        $result = $db->query("SELECT Products.* FROM Products");
-        $array = $result->fetch_all(MYSQLI_ASSOC);
         foreach ($array as & $data) {
             $tagsarray = array();
-            $productsarray = array();
+            $productsarray = array(); 
             foreach ($producttags as $tags) {
                 if ($tags['productID'] == $data['productID']) {
                     array_push($tagsarray, $tags['tag']);
@@ -27,25 +27,38 @@ function get_products($id) {
         }
         return json_encode($array);
     } elseif ($id > 0) {
-        $result = $db->query("SELECT Products.* FROM Products WHERE productID=$id");
-        if(mysqli_num_rows($result)==1){
-        $array = $result->fetch_assoc();
-        $array["tags"] = get_tag($array['productID']);
-        $array["images"] = get_product_image($array['productID']);
-        return json_encode($array);}
-        else{
-        	echo "Item not found!";
+    	$product = json_encode(array());
+    	foreach($array as &$result){
+    	if($result['productID']==$id){
+    	$product = $result;
+        $tagsarray = array();
+        $productsarray = array(); 
+        foreach ($producttags as $tags) {
+            if ($tags['productID'] == $result['productID']) {
+                    array_push($tagsarray, $tags['tag']);
+            }
         }
+        foreach ($productimgs as $images) {
+            if ($images['pid'] == $result['productID']) {
+                    array_push($productsarray, $images['image_url']);
+            }
+        }
+        $product['tags'] = $tagsarray;
+        $product['images'] = $productsarray;
+        break;
+        }
+    }
+        return json_encode($product);
     }
 }
 
 function get_product_image($pid) {
     $db = get_db_connection();
-    $result = $db->query("SELECT Products.productID as productid, image_url FROM ProductImages,Products WHERE Products.productID = ProductImages.productID")->fetch_all(MYSQLI_ASSOC);
+    $result = $db->query("SELECT * FROM ProductImages")->fetch_all(MYSQLI_ASSOC);
     $array = array();
-    foreach ($result as $key => $value) {
-        if ($value['productid'] == $pid) {
-            array_push($array, $value['image_url']);
+    foreach ($result as $data) {
+        if ($data['productID'] == $pid) {
+            array_push($array, $data['image_url']);
         }
     }
     return $array;
@@ -65,19 +78,59 @@ function get_tag($id) {
 
 function add_product($quantity, $ownerID, $productName, $descriptionText, $productPrice, $dimensions, $color, $modelname) {
     $db = get_db_connection();
-    $result = $db->query("INSERT INTO Products(quantity,ownerID,productName,descriptionText,productPrice,dimensions,color,modelname)VALUES($quantity,$ownerID,$productName,$descriptionText,$productPrice,$dimensions,$color,$modelname)");
+    $result = $db->query("INSERT INTO 
+    	Products(quantity,ownerID,productName,descriptionText,productPrice,dimensions,color,modelname)
+    	VALUES($quantity,$ownerID,'$productName','$descriptionText',$productPrice,'$dimensions','$color','$modelname')");
+    $err = $db->error;
+$echo_array = array("Accepted" => false, "Reason" => ""); 
+if($err == NULL)
+{
+   $echo_array["Accepted"] = true;
+}
+else {
+  $echo_array["Reason"] = $err;
+}
+echo json_encode($echo_array);	
 }
 
 function remove_product($id) {
     $db = get_db_connection();
-    $result = $db->query("DELETE FROM Products WHERE productID=$id");
+    $db->query("DELETE 
+    	FROM 
+    	Products 
+    	WHERE 
+    	productID='$id'");
+    $err = $db->error;
+$echo_array = array("Accepted" => false, "Reason" => ""); 
+if($err == NULL)
+{
+   $echo_array["Accepted"] = true;
+}
+else {
+  $echo_array["Reason"] = $err;
+}
+echo json_encode($echo_array);	
 }
 
 if (isset($_GET['product'])) {
     echo get_products($_GET['product']);
-} else if (isset($_POST['quantity'], $_POST['ownerID'], $_POST['productName'], $_POST['descriptionText'], $_POST['productPrice'], $_POST['dimensions'], $_POST['color'], $_POST['modelname'])) {
-    add_product($_POST['quantity'], $_POST['ownerID'], $_POST['productName'], $_POST['descriptionText'], $_POST['productPrice'], $_POST['dimensions'], $_POST['color'], $_POST['modelname']);
+} else if (isset($_POST['quantity'])) {
+	$productname = $_POST['productName'];
+	$descriptionText = $_POST['descriptionText'];
+	$productprice = $_POST['productPrice'];
+	$dimensions = $_POST['dimensions'];
+	$color = $_POST['color'];
+	$modelname = $_POST['modelname'];
+	add_product($_POST['quantity'],
+     $_POST['ownerID'],
+      $productname,
+       $descriptionText, 
+    	$productprice, 
+    	$dimensions, 
+    	$color, 
+    	$modelname);
 } else if (isset($_POST['remove'])) {
-    remove_product($_POST['remove']);
+	$remove = $_POST['remove'];
+    remove_product($remove);
 }
 ?>
